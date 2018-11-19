7864edf65369e63b0493fb08ee24f3de987b79c1
NyanKiyoshi <6186720+NyanKiyoshi@users.noreply.github.com>
2018-11-19 07:22:49 +0100

    Fixes and moved the message receiver into a thread (v1.3)
    - Fix the server's handling of non properly closed client connections,
      closes #6;
    - Doing a CTRL+Z on the message input, no longer crashes the client;
    - Requesting the client to kill itself, now properly closes
      the client socket. C# is not climbing back the stack or cleaning
      disposables when getting killed. This case is now handled;
    - No longer a allow a full command name input, to allow the user
      to quickly send a wanted command, in only one key,
      not two (e.g.: 0 + Return);
    - The default endpoint configuration is now a method,
      which is the proper, and clean way to do;
    - The argument parsing, now handles (again) empty arguments and, thus,
      Main() does not have to check anything anymore. Making the code
      cleaner;
    - No longer logging whenever an issue occurred while receiving a message
      from the client socket (e.g. timeout). This could,
      and may will be replaced later by a server status
      feedback on the client;
    - Simplified the client's SendMessage, making it cleaner;
    - The whole client can now be thread safe and cleanly independent,
      as the client is now a object that can get destroyed without exiting
      the application, or depending on the application.

---
+++ Client/DisposableClient.cs
@@ -0,0 +1,160 @@
+using System;
+using System.Data;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Shared;
+
+namespace Client {
+    /// <inheritdoc />
+    /// <summary>
+    /// A disposable chat client, that auto
+    /// </summary>
+    public class DisposableClient : IDisposable {
+        /// <summary>
+        /// The client's forever message listening task.
+        /// </summary>
+        private Thread _messageListeningThread;
+
+        /// <summary>
+        /// The polling time (in <tt>us</tt>) on the <see cref="ClientSocket"/>
+        /// </summary>
+        private const int POLLING_TIME_MICROSECONDS = 20000;
+
+        /// <summary>
+        /// The server endpoint to which we want to send data.
+        /// </summary>
+        public readonly IPEndPoint ServerEndpoint;
+
+        /// <summary>
+        /// The invoked event whenever a message is received from the server.
+        /// </summary>
+        ///
+        /// <param name="chatMessage">
+        /// The received message.
+        /// </param>
+        /// <param name="remoteEndpoint">
+        /// The remote endpoint from where the message came (the server, supposedly).
+        /// </param>
+        public delegate void OnMessageReceived(ChatMessage chatMessage, EndPoint remoteEndpoint);
+
+        /// <summary>
+        /// The event that gets invoked whenever a message is received
+        /// from the given server.
+        /// </summary>
+        public event OnMessageReceived MessageReceivedEvent;
+
+        /// <summary>
+        /// The client's state, whether it should be stopping or keep on running.
+        /// </summary>
+        public bool IsActive { get; private set; } = true;
+
+        /// <summary>
+        /// The client's socket that it's using to communicate with the server.
+        /// Storing this value as a global will allow us to retrieve data from
+        /// the server later on.
+        /// </summary>
+        public Socket ClientSocket;
+
+        /// <summary>
+        /// Initialize and start a disposable client that will listen on a given <see cref="serverEndpoint"/>.
+        /// Then will get clean up everything through the <see cref="Dispose"/> method.
+        /// </summary>
+        /// <param name="serverEndpoint">The server's endpoint to be listened on for new messages.</param>
+        public DisposableClient(IPEndPoint serverEndpoint) {
+            this.ServerEndpoint = serverEndpoint;
+            this.Start();
+        }
+
+        /// <summary>
+        /// Start a disposable client listening on a given server endpoint (<see cref="ServerEndpoint"/>)
+        /// for new messages in a separated thread.
+        /// And send any provided messages using <see cref="SendMessage"/> to the server.
+        /// </summary>
+        private void Start() {
+            this.ClientSocket =
+                new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            // Linking the socket to the communication point
+            this.ClientSocket.Bind(new IPEndPoint(IPAddress.Any, 0));
+
+            // Check for messages and prompt the user what to do (continue or stop)
+            this._messageListeningThread = new Thread(ReceiveMessagesForEver);
+            this._messageListeningThread.Start();
+        }
+
+        /// <summary>
+        /// Check for messages to be received,
+        /// keep the method blocked for <see cref="POLLING_TIME_MICROSECONDS"/> microseconds.
+        ///
+        /// If a message was received, invoke the <see cref="MessageReceivedEvent"/>.
+        /// </summary>
+        private void WaitForMessage() {
+            // Check if data is available to be received.
+            // Stop looking for messages after a few given micro-seconds.
+            while (this.ClientSocket.Poll(POLLING_TIME_MICROSECONDS, SelectMode.SelectRead)) {
+                EndPoint remoteEndPoint = null;
+                try {
+                    // Wait for a message, retrieve it and decode it
+                    var receivedMessage = IPUtils.ReceiveMessage(this.ClientSocket, out remoteEndPoint);
+
+                    // Propagate the received message
+                    this.MessageReceivedEvent?.Invoke(receivedMessage, remoteEndPoint);
+                }
+                catch (SyntaxErrorException) {
+                    Console.WriteLine(
+                        "Warning: received an invalid message from {0}", remoteEndPoint);
+                }
+                catch (SocketException) {
+                    // Wait a little before retrying (in milliseconds)
+                    Thread.Sleep(100);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Attempt to receive messages from the server,
+        /// will throw <see cref="SocketError"/> if it's unable to connect to the server.
+        /// </summary>
+        private void ReceiveMessagesForEver() {
+            // While this task is needed, let it look for messages
+            while (this.IsActive) {
+                this.WaitForMessage();
+            }
+
+            // Log that the task just finished
+            Console.Error.WriteLine("Stopped listening for new messages.");
+        }
+
+        /// <summary>
+        /// Send a message to the server using the <see cref="ClientSocket"/>.
+        /// </summary>
+        /// <param name="chatMessage"></param>
+        public void SendMessage(ChatMessage chatMessage) {
+            IPUtils.SendMessage(this.ClientSocket, chatMessage, this.ServerEndpoint);
+        }
+
+        /// <inheritdoc />
+        /// <summary>
+        /// Flag the <see cref="IDisposable"/> implementation
+        /// as to stop listening on the server for new messages
+        /// and wait for the sub thread(s) to handle the exit flag.
+        /// Then, close the <see cref="ClientSocket"/>.
+        /// </summary>
+        public void Dispose() {
+            // Flag the application to stop itself
+            this.IsActive = false;
+            Console.Error.WriteLine();
+
+            // Wait for the thread to stop listening for new messages
+            if (this._messageListeningThread.IsAlive) {
+                Console.Error.WriteLine("Waiting for the client to stop listening...");
+                this._messageListeningThread.Join();
+            }
+
+            // Close the socket
+            Console.Error.WriteLine("Closing the connection...");
+            this.ClientSocket.Close();
+        }
+    }
+}
--- Shared/DefaultConfig.cs
+++ Shared/DefaultConfig.cs
@@ -1,8 +1,19 @@
 using System.Net;
 
 namespace Shared {
+    /// <summary>
+    /// The default client and server shared config.
+    /// </summary>
     public static class DefaultConfig {
         public const short DEFAULT_SERVER_PORT = 6000;
         public static readonly IPAddress DEFAULT_SERVER_HOST = IPAddress.Loopback;
+
+        /// <summary>
+        /// Returns the default server's endpoint config.
+        /// </summary>
+        /// <returns>The default server's endpoint.</returns>
+        public static IPEndPoint GetDefaultEndPoint() {
+            return new IPEndPoint(DEFAULT_SERVER_HOST, DEFAULT_SERVER_PORT);
+        }
     }
 }
